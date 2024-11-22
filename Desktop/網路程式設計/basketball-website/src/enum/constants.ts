dc6fbde4289908a53280cd26e408954109e8f259
TKU-FCP-412637141 <sunmimi28281130@gmail.com>
2024-11-22 12:48:43 +0800

    Delete Desktop/網路程式設計/basketball-website directory

--- Desktop/網路程式設計/basketball-website/src/enum/constants.ts
+++
@@ -1,7 +0,0 @@
-export enum Position {
-  PG = "控球後衛",
-  SG = "得分後衛",
-  SF = "小前鋒",
-  PF = "大前鋒",
-  C = "中鋒"
-}
--- Desktop/網路程式設計/basketball-website/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  css: {
-    postcss: './postcss.config.cjs'  // 如果用方案2，要改這裡的路徑
-  },
-})
